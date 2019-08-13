975272ccb5213706228104b84dd751818af89337
Madalina Jantea <madalina.jantea@gmail.com>
2019-08-13 02:07:10 -0700

    assignment_express

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+
 router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
+    res.send("Welcome to Bloccit");
+  });
+
+router.get("/marco", (req, res, next) => {
+  res.send("polo");
 });
 
 module.exports = router;
