1631980922b1ea4c71a48c717651f62b38375d4a
Madalina Jantea <madalina.jantea@gmail.com>
2019-08-13 20:00:41 -0700

    checkpoint-2981

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
+const appConfig = require("./config/main-config.js");
 const express = require("express");
 const app = express();
 const routeConfig = require("./config/route-config.js");
 
+appConfig.init();
 routeConfig.init(app);
+
 module.exports = app;
+
+
