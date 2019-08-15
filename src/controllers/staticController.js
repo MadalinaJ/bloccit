47128f1835a005b69e2d2c9a1b7cfb796a6de15e
Madalina Jantea <madalina.jantea@gmail.com>
2019-08-14 18:37:03 -0700

    modif path for about.ejs

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -3,7 +3,10 @@ module.exports = {
       res.render("static/index", {title: "Welcome to Bloccit"});
   },
 
-  about(req, res, next){
-      res.render("static/about");
+ about(req, res, next){
+    res.render("static/about", {title: "About"});
   }
+  // about(req, res, next){
+  //     res.render("static/about");
+  // }
 }
