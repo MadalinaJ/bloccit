cd21eb265144bcf85aa4428df46bf0f872818881
Madalina Jantea <madalina.jantea@gmail.com>
2019-08-15 13:30:43 -0700

    assignment-2997

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,10 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    index(req, res, next){
-        res.render("static/index", {title: "Welcome to Bloccit"});
-    }
-  }
-=======
   index(req, res, next){
       res.render("static/index", {title: "Welcome to Bloccit"});
   },
@@ -12,8 +6,4 @@ module.exports = {
  about(req, res, next){
     res.render("static/about", {title: "About"});
   }
-  // about(req, res, next){
-  //     res.render("static/about");
-  // }
 }
->>>>>>> assignment_2995_routing
--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -16,6 +16,11 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "topicId",
       as: "banners",
     });
+
+  Topic.hasMany(models.Rule, {
+      foreignKey: "topicId",
+      as: "rules",
+    });
   };
   return Topic;
 };
