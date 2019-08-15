f7c2aaf5cea25778674aac749cdddb1c99677e3c
Madalina Jantea <madalina.jantea@gmail.com>
2019-08-15 12:23:36 -0700

    checkpoint-2997-topic1

---
+++ src/db/models/topic.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Topic = sequelize.define('Topic', {
+    title: {
+      type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+           type: DataTypes.STRING,
+           allowNull: false
+        }
+      }, {});
+  Topic.associate = function(models) {
+    // associations can be defined here
+    Topic.hasMany(models.Banner, {
+      foreignKey: "topicId",
+      as: "banners",
+    });
+  };
+  return Topic;
+};
