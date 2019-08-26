345de7ec1b5dd174eee21779188c4db03c20df0c
Madalina Jantea <madalina.jantea@gmail.com>
2019-08-26 14:17:34 -0700

    checkpoint-3002-comments

--- src/db/migrations/20190826211139-create-banner.js
+++ src/db/migrations/20190826211139-create-banner.js
@@ -29,7 +29,7 @@ module.exports = {
           model: "Topics",
           key: "id",
           as: "topicsId",
-        }
+        },
       }
     });
   },
--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
   Topic.associate = function(models) {
     // associations can be defined here
     Topic.hasMany(models.Banner, {
-      foreignKey: "topicId",
+      foreignKey: "topicsId",
       as: "banners",
     });
 
